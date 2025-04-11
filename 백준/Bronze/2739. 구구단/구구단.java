5c935bc75081a41d5201b3b81b8ebc7882a025ad
wjmj0720 <wjmj0720@gmail.com>
2025-04-11 21:36:20 +0900

    [Bronze V] Title: 구구단, Time: 200 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,97 @@
+
+import java.util.Scanner;
+
+public class Main {
+    //1. ? 단 입력받기 -> 스캐너
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int i;
+    int result =0;
+    //2. ?단 세팅하기 -> case
+    //3. ? 단 출력하기 -> case에 print
+    switch(a){
+        case 1 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+            }
+            break;
+
+        case 2 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+
+            }
+            break;
+
+        case 3 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 4 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 5 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 6 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 7 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 8 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+            }
+            break;
+
+        case 9 :
+            for(i=1;i<10;i++){
+                result = a * i ;
+                System.out.println(a+ " * "+ i +" = "+ result);
+
+
+            }
+            break;
+
+        default :
+            break;
+
+    }
+
+
+
+
+    }
+}
